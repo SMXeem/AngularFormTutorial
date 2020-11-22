18598eee6e0dd1ca4c7f385b22072a6ebac3a062
Sayed Al Zawad XEEM <sayedxeem24@gmail.com>
2020-11-22 20:10:47 +0600

    Data bindling in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularFormTutorial';
   topics=['Angular','React','Vue'];
+  userModel=new User('Rob','s@gmail.cpm',3242432,'Vue','evening',true);
 }
